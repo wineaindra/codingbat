788a016527f8068892b4310aa04ba8e333628268
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-05 16:33:58 +0630

    finished sum_double

---
+++ ruby/sum_double.rb
@@ -0,0 +1,13 @@
+def sum_double(first_num,second_num)
+	if first_num==second_num
+		result=(first_num+second_num)*2
+		return result
+
+	elsif 
+		result=first_num+second_num
+		return result
+	end	
+end
+
+puts sum_double(5,6);
+puts sum_double(5,5);
