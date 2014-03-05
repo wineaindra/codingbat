e049c0d91d51d89aa78721c63acd2185802e31dc
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-05 15:12:15 +0630

    Finished monkey_trouble

---
+++ ruby/monkey_trouble.rb
@@ -0,0 +1,12 @@
+def monkey_trouble(a_smile, b_smile)
+	if (a_smile==true && b_smile==true)
+		return true
+	elsif (a_smile==false && b_smile==false)
+		return true
+	else
+		return false	
+	end	
+end
+puts monkey_trouble(false,false) ? "In trouble": "Not in trouble"
+puts monkey_trouble(true,true) ? "In trouble" : "Not in trouble"
+puts monkey_trouble(true,false) ? "In trouble" : "Not in trouble"
