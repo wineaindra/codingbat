dabfaa5dc1900a301bca785df2f8a9c4129510d4
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-06 14:08:48 +0630

    shorten sleep_in

--- ruby/sleep_in.rb
+++ ruby/sleep_in.rb
@@ -1,7 +1,5 @@
 def sleep_in(is_weekday,is_on_vacation)
-	if is_weekday==false || is_on_vacation==true
-		return true	
-	end 
+	return !is_weekday || is_on_vacation
 end
 puts sleep_in(false, false) ? "pass" : "fail"
 puts sleep_in(true, false) ? "fail" : "pass" 
