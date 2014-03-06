5cce81e4b9ce3c770ebed315eb2b3e7167d8b7ad
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-06 16:03:29 +0630

    parrort_trouble

--- ruby/parrot_trouble.rb
+++ ruby/parrot_trouble.rb
@@ -1,12 +1,8 @@
 def parrot_trouble(is_talking,time)
-	if is_talking==true && (time<7 || time>20)
-		return true
-	else
-		return false
-	end	
+	return is_talking && (time<7 || time>20)
 end
-puts parrot_trouble(true,6) ? "True" : "False"
-puts parrot_trouble(true,7) ? "True" : "False"
-puts parrot_trouble(false,6) ? "True" : "False"
-puts parrot_trouble(true,23) ? "True" : "False"
-puts parrot_trouble(false,23) ? "True" : "False"
+puts parrot_trouble(true,6) ? "pass" : "fail"
+puts parrot_trouble(true,7) ? "fail" : "pass"
+puts parrot_trouble(false,6) ? "fail" : "pass"
+puts parrot_trouble(true,23) ? "pass" : "fail"
+puts parrot_trouble(false,23) ? "fail" : "pass"
