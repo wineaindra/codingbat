04e942a7e3e0bbe6d79647108a02598af3aa039c
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-06 14:17:36 +0630

    shorten sum_double

--- ruby/sum_double.rb
+++ ruby/sum_double.rb
@@ -1,12 +1,6 @@
 def sum_double(first_num,second_num)
-	if first_num==second_num
-		result=(first_num+second_num)*2
-		return result
-		elsif 
-		result=first_num+second_num
-		return result
-	end	
+	return (first_num+second_num)*(first_num==second_num ? 2 : 1) 
 end
-puts sum_double(1,2)
-puts sum_double(3,2)
-puts sum_double(2,2)
+puts sum_double(1,2)==3 ? "pass" : "fail"
+puts sum_double(3,2)==5 ? "pass" : "fail"
+puts sum_double(2,2)==8 ? "pass" : "fail"
