d941b2986d3ace760ef71e9bfa1bf59ed4775b3c
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-06 16:03:03 +0630

    shorten makes10

--- ruby/makes10.rb
+++ ruby/makes10.rb
@@ -1,13 +1,9 @@
 def makes10(a,b)
-	if a==10 || b==10 || a+b==10
-		return true
-	else
-		return false	
-	end	
+	return a==10 || b==10 || a+b==10
 end
-puts makes10(9,10) ? "True" : "False"
-puts makes10(9,9) ? "True" : "False"
-puts makes10(9,1) ? "True" : "False"
-puts makes10(1,2) ? "True" : "False"
-puts makes10(5,5) ? "True" : "False"
-puts makes10(10,10) ? "True" : "False"
+puts makes10(9,10) ? "pass" : "fail"
+puts makes10(9,9) ? "fail" : "pass"
+puts makes10(9,1) ? "pass" : "fail"
+puts makes10(1,2) ? "fail" : "pass"
+puts makes10(5,5) ? "pass" : "fail"
+puts makes10(10,10) ? "pass" : "fail"
