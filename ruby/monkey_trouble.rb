ba0cd575df6be384020eecb2c9e6147b667cc4e2
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-06 14:08:23 +0630

    shorten monkey_trouble

--- ruby/monkey_trouble.rb
+++ ruby/monkey_trouble.rb
@@ -1,12 +1,6 @@
 def monkey_trouble(a_smile, b_smile)
-	if (a_smile==true && b_smile==true)
-		return true
-		elsif (a_smile==false && b_smile==false)
-		return true
-		else
-		return false	
-	end	
+	return (a_smile && b_smile) || !(a_smile || b_smile) 
 end
-puts monkey_trouble(false,false) ? "True": "False"
-puts monkey_trouble(true,true) ? "True" : "False"
-puts monkey_trouble(true,false) ? "True" : "False"
+puts monkey_trouble(false,false) ? "pass": "fail"
+puts monkey_trouble(true,true) ? "pass" : "fail"
+puts monkey_trouble(true,false) ? "fail" : "pass"
