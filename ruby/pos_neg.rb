91684220b17717bebcd64275bb5d1b12dd3ff160
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-07 11:58:54 +0630

    shorten pos_neg

--- ruby/pos_neg.rb
+++ ruby/pos_neg.rb
@@ -1,15 +1,9 @@
 def pos_neg(a,b,negative)
-	if (a<0 && b>0 && negative==false) || (a>0 && b<0 && negative==false)
-		return true
-		elsif a<0 && b<0 && negative==true
-		return true
-	else
-		return false						
-	end	
+	return (a<0 && b>0 && negative==false) || (a>0 && b<0 && negative==false) || (a<0 && b<0 && negative)
 end
-puts pos_neg(1,-1,false) ? "True" : "False"
-puts pos_neg(-2,3,false) ? "True" : "False"
-puts pos_neg(-4,-5,true) ? "True" : "False"
-puts pos_neg(1,1,false) ? "True" : "False"
-puts pos_neg(3,-9,true) ? "True" : "False"
+puts pos_neg(1,-1,false) ? "pass" : "fail"
+puts pos_neg(-2,3,false) ? "pass" : "fail"
+puts pos_neg(-4,-5,true) ? "pass" : "fail"
+puts pos_neg(1,1,false) ? "fail" : "pass"
+puts pos_neg(3,-9,true) ? "fail" : "pass"
 
