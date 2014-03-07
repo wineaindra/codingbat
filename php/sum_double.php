2b75eb4cd45690f3cad278d9da9e781e09cc75f0
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-07 10:12:28 +0630

    shorten sum_double

--- php/sum_double.php
+++ php/sum_double.php
@@ -1,18 +1,9 @@
 <?php
 	function sum_double($first_num, $second_num)
 	{
-		if ($first_num==$second_num)
-		{
-			$result=($first_num+$second_num)*2;
-			return $result;
-		}
-		else
-		{
-			$result=$first_num+$second_num;
-			return $result;
-		}
+		return ($first_num+$second_num) * ($first_num==$second_num ? 2 : 1);
 	}
-	echo sum_double(1,2); echo "\n";
-	echo sum_double(3,2); echo "\n";
-	echo sum_double(2,2); echo "\n";
+	echo sum_double(1,2)==3 ? "pass" : "fail"; echo "\n";
+	echo sum_double(3,2)==5 ? "pass" : "fail"; echo "\n";
+	echo sum_double(2,2)==8 ? "pass" : "fail"; echo "\n";
 ?>
