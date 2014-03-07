3f18419182da510e78482b175e568c621551c686
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-07 10:32:15 +0630

    shorten diff21

--- php/diff21.php
+++ php/diff21.php
@@ -1,19 +1,10 @@
 <?php
 	function diff21($n)
 	{
-		if ($n > 21)
-		{
-			$result=($n-21)*2;
-			return $result;
-		}
-		else
-		{
-			$result=(21-$n);
-			return $result;
-		}
+		return (abs($n-21) * ($n > 21 ? 2 : 1)); 
 	}
-	echo diff21(19); echo "\n";
-	echo diff21(10); echo "\n";
-	echo diff21(21); echo "\n";
-	echo diff21(25); echo "\n";
+	echo diff21(19)==2 ? "pass" : "fail"; echo "\n";
+	echo diff21(10)==11 ? "pass" : "fail"; echo "\n";
+	echo diff21(21)==0 ? "pass" : "fail"; echo "\n";
+	echo diff21(25)==8 ? "pass" : "fail"; echo "\n";
 ?>
