0c8423750a3b4f2e2c1746553a832e7317d331c4
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-07 10:50:32 +0630

    shorten parrot_trouble

--- php/parrot_trouble.php
+++ php/parrot_trouble.php
@@ -1,15 +1,11 @@
 <?php
 	function parrot_trouble($is_talking, $time)
 	{
-		
-		if($is_talking==true && ($time<7 || $time>20))
-			return true;
-		else
-			return false;
+		return ($is_talking && ($time < 7 || $time > 20));
 	}
-	echo parrot_trouble(true,6) ? "True" : "False"; echo "\n";
-	echo parrot_trouble(true,7) ? "True" : "False"; echo "\n";
-	echo parrot_trouble(false,6) ? "True" : "False"; echo "\n";
-	echo parrot_trouble(true,23) ? "True" : "False"; echo "\n";
-	echo parrot_trouble(false,23) ? "True" : "False"; echo "\n";
+	echo parrot_trouble(true,6) ? "pass" : "fail"; echo "\n";
+	echo parrot_trouble(true,7) ? "fail" : "pass"; echo "\n";
+	echo parrot_trouble(false,6) ? "fail" : "pass"; echo "\n";
+	echo parrot_trouble(true,23) ? "pass" : "fail"; echo "\n";
+	echo parrot_trouble(false,23) ? "fail" : "pass"; echo "\n";
 ?>
