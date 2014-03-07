d85840f11801a516d365c26c10f6d7b62afd021f
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-07 10:03:57 +0630

    shorten sleep_in

--- php/sleep_in.php
+++ php/sleep_in.php
@@ -2,9 +2,7 @@
 
 	function sleep_in($is_weekday,$is_on_vacation)
 	{
-
-		if ($is_weekday==false || $is_on_vacation==true) 
-			return true;
+		return (!$is_weekday || $is_on_vacation);
 	}
 echo sleep_in(False, False) ? "pass" : "fail"; echo "\n";
 echo sleep_in(True, False) ? "fail" : "pass"; echo "\n";
