4ae0cf3d835da13af2f8f09393c7322214ab591e
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-10 10:46:59 +0630

    finished missing_char

---
+++ php/missing_char.php
@@ -0,0 +1,10 @@
+<?php 
+	function missing_char($string,$n)
+	{
+		$word= $string{$n};
+		return str_replace($word,"",$string);
+	}
+	echo missing_char("kitten",1)=="ktten" ? "pass" : "fail"; echo "\n";
+	echo missing_char("kitten",0)=="itten" ? "pass" : "fail"; echo "\n";
+	echo missing_char("kitten",4)=="kittn" ? "pass" : "fail"; echo "\n";
+?>
