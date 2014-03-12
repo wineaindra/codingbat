3522fe245e5e814692be7d5822d8fa7eba48d62d
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-12 13:49:35 +0630

    test array_count9

--- ruby/array_count9.rb
+++ ruby/array_count9.rb
@@ -1,6 +1,6 @@
 def array_count9(arr)
 	return arr.count(9)
 end
-puts array_count9([1,2,9])
-puts array_count9([1,9,9])
-puts array_count9([1,9,9,3,9])
+puts array_count9([1,2,9])==1 ? "pass" : "fail"
+puts array_count9([1,9,9])==2 ? "pass" : "fail"
+puts array_count9([1,9,9,3,9])==3 ? "pass" : "fail"
