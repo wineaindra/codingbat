fd85be692b9f38e51bede4da6e296ae34ec5c226
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-14 11:40:37 +0630

    finished string_spolosion but need to do pass and fail

--- php/string_spolosion.php
+++ php/string_spolosion.php
@@ -1,9 +1,14 @@
 <?php
-	$str="Code";
-	echo $str[0];
-	echo $str[0];
-	echo $str[1];
-	echo $str[0];
-	echo $str[1];
-	echo "\n";
+	function string_spolosion($str)
+	{
+		$result="";
+		for ($i=0; $i < strlen($str) ; $i++) 
+		{ 
+			$result.=$str[$i];
+			echo $result;
+		}
+	}
+	string_spolosion("Code"); echo "\n";
+	string_spolosion("abc"); echo "\n";
+	string_spolosion("ab"); echo "\n";
 ?>
