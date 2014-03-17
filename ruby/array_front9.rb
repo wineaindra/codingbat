265ea76cab6c5a20804c3649f34f16aa92ac8581
Win Eaindra <wineaindrabathaung@rebbiz.com>
2014-03-17 15:04:39 +0630

    write rspec for array_front9

--- ruby/array_front9.rb
+++ ruby/array_front9.rb
@@ -1,6 +1,3 @@
 def array_front9(arr)
 	return arr[0..3].include?(9)
 end
-puts array_front9([1, 2, 9, 3, 4]) ? "pass" : "fail"
-puts array_front9([1, 2, 3, 4, 9]) ? "fail" : "pass"
-puts array_front9([1, 2, 3, 4, 5]) ? "fail" : "pass"
--- ruby/spec/spec.rb
+++ ruby/spec/spec.rb
@@ -4,6 +4,7 @@ require_relative "../diff21"
 require_relative "../sum_double"
 require_relative "../string_times"
 require_relative "../array123"
+require_relative "../array_front9"
 
 describe "sleep_in" do
 	it "should return true if we are on vacation" do
@@ -69,6 +70,15 @@ describe "array123" do
 	end
 	it "should return false if 1,2,3 is not appear in the array somewhere" do
 		array123([1, 1, 2, 4, 1]).should be_false
+	end
+end
+
+describe "array_front9" do
+	it "should return True if one of the first 4 elements in the array is a 9" do
+		array_front9([1, 2, 9, 3, 4]). should be_true
+	end
+	it "should return False if one of the first 4 elements in the array is not 9" do
+		array_front9([1, 2, 3, 4, 9]).should be_false
 	end
 end
 
